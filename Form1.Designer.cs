1fcb7152d5b7acae384f9a45472615300e877365
lengocquy <1651012149quy@ou.edu.vn>
2018-10-11 07:58:04 +0700

    thay doi button

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -50,7 +50,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
-            this.button1.Text = "Cộng";
+            this.button1.Text = "Cộng1";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
